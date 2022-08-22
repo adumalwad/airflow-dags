7ebeecc645ac6cf162c99090cb9476045edf5508
Anup Dumalwad <adumalwad@gmail.com>
2022-08-22 16:34:25 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime
+import subprocess
+
+def excuteC():
+	s = subprocess.check_call("gcc 3.c -o out1;./out1", shell = True)
+	print(", return code", s)
+
+def executeCpp():
+	s = subprocess.check_output("g++ 2.cpp -o out2;./out2", shell = True)
+	print(s.decode("utf-8"))
+
+def Execute_Python1():
+    print("THIS IS PYTHON1 OUTPUT")
+
+def Execute_Python2():
+    print("THIS IS PYTHON2 OUTPUT")
+
+with DAG("anup_dag_5", start_date=datetime(2022, 1, 1),
+    schedule_interval="@daily", catchup=False) as dag:
+
+        Execute_Python11 = PythonOperator(
+            task_id="Execute_Python11",
+            python_callable=Execute_Python1
+        )
+        Execute_C = PythonOperator(
+            task_id="Execute_C",
+            python_callable=excuteC
+        )
+        Execute_CPP = PythonOperator(
+            task_id="Execute_CPP",
+            python_callable=executeCpp
+        )
+        Execute_Python12 = PythonOperator(
+            task_id="Execute_Python12",
+            python_callable=Execute_Python2
+        )
+
+        Execute_Python11 >> [Execute_C,Execute_CPP] >> Execute_Python12
---
+++ 2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=90, b=30x;
+	cin >> a >> b;
+	cout << "Hello World from C++.Values are:" << a << " " << b;
+	return 0;
+}
