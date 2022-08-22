c188727552ab1e9de452e8750b315c7b7b54436b
Anup Dumalwad <adumalwad@gmail.com>
2022-08-22 18:40:55 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ import subprocess
 def excuteC():
 	#s = SubprocessHook.run_command(self,"gcc 3.c -o out1;./out1",env=None, output_encoding='utf-8', cwd=None)
 	#s = subprocess.check_call("gcc 3.c -o out1;./out1", shell = True)
-	s = subprocess.check_call("./out1", shell = True)
+	s = subprocess.check_call("ls", shell = True)
 	print(", return code", s)
 
 def executeCpp():
